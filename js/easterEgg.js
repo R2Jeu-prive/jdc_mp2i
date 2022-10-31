c3a2927d0023d9371bdba28c1e6f9fab8ef6cdb5
Pierre <R2Jeu.prive@gmail.com>
2022-10-31 22:28:19 +0100

    added easter egg

---
+++ js/easterEgg.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    let pass = "mp2i";
+    let progress = 0;
+    $(document).on("keydown", function(e){
+        if(e.key == pass[progress]){
+            progress += 1;
+            if(progress >= 4){
+                window.open("https://www.youtube.com/watch?v=xvFZjo5PgG0", "_blank");//easter egg
+            }
+        }
+    })
+});
